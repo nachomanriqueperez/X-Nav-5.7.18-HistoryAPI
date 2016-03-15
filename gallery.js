60b448c7414514f2d2d57963b0fa5c197adf3b4c
nachomanriqueperez <joseim@zeta40.aulas.gsyc.es>
2016-03-15 14:52:58 +0100

    Hecho API AJAX

--- gallery.js
+++ gallery.js
@@ -5,7 +5,7 @@ function supports_history_api() {
 function swapPhoto(href) {
   var req = new XMLHttpRequest();
   req.open("GET",
-           "http://gsyc.es/~grex/history_api/gallery/" +
+           "./gallery/" +
              href.split("/").pop(),
            false);
   req.send(null);
@@ -40,4 +40,3 @@ window.onload = function() {
     }, false);
   }, 1);
 }
-
